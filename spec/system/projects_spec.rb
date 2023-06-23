55c7b4f40fccd9072d7bb547797a83e3a9321d9a
t-kuzuma <t-kuzuma@ruby-dev.jp>
2023-06-23 11:18:41 +0900

    8章

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,26 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
-  scenario "user creates a new project" do
-    user = FactoryBot.create(:user)
-    # using our custom login helper:
-    # sign_in_as user
-    # or the one provided by Devise:
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
     sign_in user
-
     visit root_path
+  end
 
+  def create_project
+    click_link "New Project"
+    fill_in "Name", with: "Test Project"
+    fill_in "Description", with: "Trying out Capybara"
+    click_button "Create Project"
+  end
+
+  def edit_project
+    click_link "Edit"
+    fill_in "Name", with: "Edit Project"
+    click_button "Update Project"
+  end
+
+  scenario "user creates a new project" do
     expect {
-      click_link "New Project"
-      fill_in "Name", with: "Test Project"
-      fill_in "Description", with: "Trying out Capybara"
-      click_button "Create Project"
-
+      create_project
       aggregate_failures do
         expect(page).to have_content "Project was successfully created"
         expect(page).to have_content "Test Project"
         expect(page).to have_content "Owner: #{user.name}"
       end
     }.to change(user.projects, :count).by(1)
+  end
+
+  scenario "user edits a project" do
+    create_project
+    edit_project
+    expect(page).to have_content "Project was successfully updated"
+    expect(page).to have_content "Edit Project"
   end
 end
