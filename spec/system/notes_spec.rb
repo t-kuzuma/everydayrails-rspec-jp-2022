47767481f781973d57cc7b0b02050e74a47db150
t-kuzuma <t-kuzuma@ruby-dev.jp>
2023-07-03 10:31:10 +0900

    ログイン部分の可読性をあげる

--- spec/system/notes_spec.rb
+++ spec/system/notes_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Notes", type: :system do
   scenario "user creates a new note" do
-    user = FactoryBot.create(:user)
+    user = FactoryBot.create(:user, email: 'alice@example.com', password: 'pass1234')
     project = FactoryBot.create(:project, owner: user, name: "Test Project")
 
     visit root_path
     click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
+    fill_in "Email", with: 'alice@example.com'
+    fill_in "Password", with: 'pass1234'
     click_button "Log in"
     expect(page).to have_content "Signed in successfully."
 
