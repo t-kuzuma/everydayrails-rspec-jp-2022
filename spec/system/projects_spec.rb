18a97e4eb514535f826d7ec1212cc38d81267c43
t-kuzuma <t-kuzuma@ruby-dev.jp>
2023-07-03 11:32:10 +0900

    引数でデータを渡して見やすく editも同様

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -8,34 +8,34 @@ RSpec.describe "Projects", type: :system do
     visit root_path
   end
 
-  def create_project
+  def create_project(options)
     click_link "New Project"
-    fill_in "Name", with: "Test Project"
-    fill_in "Description", with: "Trying out Capybara"
+    fill_in "Name", with: options[:name]
+    fill_in "Description", with: options[:description]
     click_button "Create Project"
   end
 
-  def edit_project
+  def edit_project(options)
     click_link "Edit"
-    fill_in "Name", with: "Edit Project"
+    fill_in "Name", with: options[:name]
     click_button "Update Project"
   end
 
   scenario "user creates a new project" do
     expect {
-      create_project
+      create_project(name: 'RSpec Practice', description: 'fjord bootcamp')
       aggregate_failures do
         expect(page).to have_content "Project was successfully created"
-        expect(page).to have_content "Test Project"
+        expect(page).to have_content 'RSpec Practice'
         expect(page).to have_content "Owner: #{user.name}"
       end
     }.to change(user.projects, :count).by(1)
   end
 
   scenario "user edits a project" do
-    create_project
-    edit_project
+    create_project(name: 'RSpec Practice', description: 'fjord bootcamp')
+    edit_project(name: 'Minitest Practice')
     expect(page).to have_content "Project was successfully updated"
-    expect(page).to have_content "Edit Project"
+    expect(page).to have_content "Minitest Practice"
   end
 end
